49ef5886c809b184b50b9dadd2ca2926dcadd2c8
Yeon85 <icanspeak85@gmail.com>
2025-04-09 12:13:27 +0900

    commit

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ npm install vite --save-dev
 npm install 
 
 npm run dev
+
+
+git status
+
+git add <파일명>
+# 또는 모든 변경 사항을 추가하려면
+git add .
--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,66 @@ function App() {
   const name = "ZI-A";
 
   return (
-    <div>
+    <div className="center">
      <ProfileCard
         image={profileImg}
         name="정지아"
         description="신소재를 사랑하는 개발자"
       />
-  </div>
+  
+      {/* ✅ 링크 버튼 추가 */}
+      <a
+        href="https://chapter3-react1.vercel.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          display: 'inline-block',
+          marginTop: '20px',
+          padding: '10px 20px',
+          backgroundColor: '#4CAF50',
+          color: '#fff',
+          textDecoration: 'none',
+          borderRadius: '6px'
+        }}
+      >
+        📎 CHAPTER 3 - 기초 열기
+      </a>
+      <br></br>
+        {/* ✅ 링크 버튼 추가 */}
+      <a
+        href="https://chapter3-react2.vercel.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          display: 'inline-block',
+          marginTop: '20px',
+          padding: '10px 20px',
+          backgroundColor: '#4CAF50',
+          color: '#fff',
+          textDecoration: 'none',
+          borderRadius: '6px'
+        }}
+      >
+        📎 CHAPTER 3 게시판 CRUD
+      </a>
+      <br></br>
+      <a
+        href="https://chapter3-react3.vercel.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          display: 'inline-block',
+          marginTop: '20px',
+          padding: '10px 20px',
+          backgroundColor: '#4CAF50',
+          color: '#fff',
+          textDecoration: 'none',
+          borderRadius: '6px'
+        }}
+      >
+        📎 CHAPTER 3 게시판 CRUD MYSQL
+      </a>
+    </div>
   );
 }
 
